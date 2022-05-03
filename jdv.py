2a43ef314a51e38eac48cc6e46549c52b450d99e
michhel23 <96630197+michhel23@users.noreply.github.com>
2022-05-03 02:19:48 +0200

    jdv v2
    ajoute changement de couleur, optimisation des performances, amélioration interface graphique, interactions terminal enlevées

--- jdv.py
+++ jdv.py
@@ -1,15 +1,35 @@
-from time import sleep
 from tkinter import *
-import ctypes
+from copy import deepcopy as copie
 
-def numb(t,x,y):
-    t[y+1][x+1]=1
+def numb(t,x,y,n):
+    t[y+1][x+1]=n
 
-def copie(l):
-    u=[]
-    for i in l:
-        u.append(list(i))
-    return list(u)
+def color():
+    global colorCell
+    global id
+    if id != len(rgb)-1 and id+1!=idd or id!=9 and idd!=0:
+        id+=1
+    elif id+1==idd:
+        id+=2
+    elif id==9 and idd==0:
+        id=1
+    else:
+        id=0
+    
+    colorCell=rgb[id]
+
+def colorBg():
+    global idd
+    if idd != len(rgb)-1 and idd+1!=id or idd!=9 and id!=0:
+        idd+=1
+    elif idd+1==id:
+        idd+=2
+    elif idd==9 and id==0:
+        idd=1
+    else:
+        idd=0
+    
+    base.configure(bg=rgb[idd])
 
 def start():
     global table
@@ -32,40 +52,69 @@ def start():
     table=copie(past)
     #affichage du changement
     afficherGr(table)
-    win.after(speed,start)
+    win.after(speed,go)
 
-def click(event):
+def click_gauche(event):
     x = event.x -(event.x%c)
     y = event.y -(event.y%c)
-    base.create_rectangle(x, y, x+c, y+c, fill='black')
-    numb(table,int(x/c),int(y/c))
+    base.create_rectangle(x, y, x+c, y+c, fill=colorCell,outline=colorCell)
+    numb(table,int(x/c),int(y/c),1)
 
-def go(event):
-    start() 
+def click_droit(event):
+    x = event.x -(event.x%c)
+    y = event.y -(event.y%c)
+    base.create_rectangle(x, y, x+c, y+c, fill=colorDeadCell,outline=colorDeadCell)
+    numb(table,int(x/c),int(y/c),0) 
+
+def stop():
+    global stop
+    stop=1
+
+def go():
+    global stop
+    if stop==1:
+        stop=0
+    else:
+        start()
 
 def afficherGr(t):
     base.delete(ALL)
     for y in range(1,h+1):
         for x in range(1,w+1):
-            if t[y][x]==0:
-                base.create_rectangle(x*c, y*c, x*c+c, y*c+c, fill='white',outline="white")
-            else:
-                base.create_rectangle(x*c, y*c, x*c+c, y*c+c, fill='black')
+            if t[y][x]==1:
+                base.create_rectangle(x*c, y*c, x*c+c, y*c+c, fill=colorCell,outline=colorCell)
     
 #programme princ.
-print("___________________________________________________________________________\nclick gauche>>>poser une case\nclick droit>>>start\n(action à effectuer sur la fenetre une fois les valeurs entrées)\n___________________________________________________________________________")
-speed=int(float(input("vitesse(fps):"))*1000)
-c=int(input("taille d'une case(px):"))
+id=1
+idd=8
+rgb=["red","black","blue","green","yellow","purple","cyan","orange","white","pink"]
+colorCell=rgb[id]
+colorDeadCell=rgb[idd]
+speed=200
 win=Tk()
-user32 = ctypes.windll.user32 
-w = user32.GetSystemMetrics(78)//c
-h = user32.GetSystemMetrics(79)//c
-print(w,h)
+stop=0
+#nombre de cases (largeur-hauteur)
+w = 70
+h = 50
+width=700
+height=500
+c=10
+win.configure(bg="grey")
 table=[[0 for i in range(w+2)]for i in range(h+2)]
-base = Canvas(win,width=w*c,height=h*c,bg='white')
-base.bind("<Button-1>",click)
-base.bind('<Button-3>',go)
-base.pack()
+base = Canvas(win,width=700,height=500)
+base.bind("<Button-1>",click_gauche)
+base.bind('<Button-3>',click_droit)
+base.configure(bg="white")
+base.pack(side =TOP)
+
+
+
+b1 = Button(win, text ='Go!', command =start)
+b1.pack(side =LEFT, padx =3, pady =3)
+b2 = Button(win,text="color change", command =color)
+b2.pack(side = RIGHT,padx=3,pady=3)
+b3 = Button(win,text="color bg change", command =colorBg)
+b3.pack(side = RIGHT,padx=3,pady=3)
 win.mainloop()
 
 
